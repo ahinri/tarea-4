dace3bf0eff5b5acc33323bcc8166be64373173e
ahinri <afhinrichsen@uc.cl>
2016-06-20 15:07:18 -0400

    maps

--- slack-bot/commands/accion.rb
+++ slack-bot/commands/accion.rb
@@ -1,10 +1,14 @@
 module SlackBot
   module Commands
     class Accion < SlackRubyBot::Commands::Base
-      #command 'accion' do |client, data, _match|
-      #  client.say(channel: data.channel, text: "https://maps.googleapis.com/maps/api/staticmap?size=500x500&center=santiago_de_chile")
-      match /^(?<location>*)$/ do |client, data, match|
-      	client.say(channel: data.channel, text: "https://maps.googleapis.com/maps/api/staticmap?size=500x500&center=#{match[:location]}")
+      #match /^(?<location>\w*)$/ do |client, data, match|
+      #	client.say(channel: data.channel, text: "https://maps.googleapis.com/maps/api/staticmap?size=500x500&center=#{match[:location]}")
+      scan(/([a-Z]+)/) do |client, data, stocks|
+      	string=""
+      	stocks.each do |palabra|
+      	  string=string+"_"+palabra
+      	end
+      client.say(channel: data.channel, text: "https://maps.googleapis.com/maps/api/staticmap?size=500x500&center="+string)
       end
     end
   end
