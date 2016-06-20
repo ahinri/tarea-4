2c8354b6f0906790d8217c2de9e6fe3407b360dd
ahinri <afhinrichsen@uc.cl>
2016-06-20 13:51:09 -0400

    maps

--- slack-bot/bot.rb
+++ slack-bot/bot.rb
@@ -1,6 +1,4 @@
 module SlackBot
   class Bot < SlackRubyBot::Bot
-    match /^Muestrame (?<location>\w*)\?$/ do |client, data, match|
-      client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
   end
 end
--- slack-bot/commands/accion.rb
+++ slack-bot/commands/accion.rb
@@ -2,7 +2,7 @@ module SlackBot
   module Commands
     class Accion < SlackRubyBot::Commands::Base
       command 'accion' do |client, data, _match|
-        client.say(channel: data.channel, text: '4')
+        client.say(channel: data.channel, text: 'https://maps.googleapis.com/maps/api/staticmap?size=500x500&center=santiago_de_chile')
       end
     end
   end
