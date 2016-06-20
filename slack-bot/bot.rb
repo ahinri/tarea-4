c8e000592a77099c4b1ab76c3a4a4cf6912b1733
ahinri <afhinrichsen@uc.cl>
2016-06-20 13:43:29 -0400

    maps

--- slack-bot/bot.rb
+++ slack-bot/bot.rb
@@ -1,6 +1,6 @@
 module SlackBot
   class Bot < SlackRubyBot::Bot
-    match /^(?<location>\w*)\?$/ do |client, data, match|
+    match /^Muestrame (?<location>\w*)\?$/ do |client, data, match|
       client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
   end
 end
