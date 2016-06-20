c62ac0a745d8e04b138809c860eb1f6d78a0b07b
ahinri <afhinrichsen@uc.cl>
2016-06-20 14:02:11 -0400

    maps

--- slack-bot/bot.rb
+++ slack-bot/bot.rb
@@ -1,7 +1,7 @@
 module SlackBot
   class Bot < SlackRubyBot::Bot
-  	match /^Muestrame (?<location>\w*)\?$/ do |client, data, match|
-      client.say(channel: data.channel, text: "https://maps.googleapis.com/maps/api/staticmap?size=500x500&center=#{match[:location]}")
-    end
+  	#match /^Muestrame (?<location>\w*)\?$/ do |client, data, match|
+    #  client.say(channel: data.channel, text: "https://maps.googleapis.com/maps/api/staticmap?size=500x500&center=#{match[:location]}")
+    #end
   end
 end
