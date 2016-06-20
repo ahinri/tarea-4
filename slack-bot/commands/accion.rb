348995ec764020f5e56b51fbf5839adcc04aef51
ahinri <afhinrichsen@uc.cl>
2016-06-20 12:48:01 -0400

    prueba google

--- slack-bot/commands/accion.rb
+++ slack-bot/commands/accion.rb
@@ -1,8 +1,16 @@
 module SlackBot
   module Commands
     class Accion < SlackRubyBot::Commands::Base
-      command 'accion' do |client, data, _match|
-        client.say(channel: data.channel, text: '4')
+      #command 'accion' do |client, data, _match|
+      #  client.say(channel: data.channel, text: '4')
+      match(/^(?<bot>\w*)\s(?<expression>.*)$/) do |client, data, match|
+        expression = match['expression'].strip
+        query = { query: expression }
+        query[:cx] = ENV['GOOGLE_CSE_ID'] if ENV['GOOGLE_CSE_ID']
+        result = ::Google::Search::Web.new(query).first
+        message = result.title + "\n" + result.uri
+        send_message client, data.channel, message
+      end
       end
     end
   end
