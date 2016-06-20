6a561b1ea337c79701f1fba82a28f16657645b43
ahinri <afhinrichsen@uc.cl>
2016-06-20 15:10:22 -0400

    maps

--- slack-bot/commands/accion.rb
+++ slack-bot/commands/accion.rb
@@ -1,14 +1,8 @@
 module SlackBot
   module Commands
     class Accion < SlackRubyBot::Commands::Base
-      #match /^(?<location>\w*)$/ do |client, data, match|
-      #	client.say(channel: data.channel, text: "https://maps.googleapis.com/maps/api/staticmap?size=500x500&center=#{match[:location]}")
-      scan(/([a-Z]+)/) do |client, data, stocks|
-      	string=""
-      	stocks.each do |palabra|
-      	  string=string+"_"+palabra
-      	end
-      client.say(channel: data.channel, text: "https://maps.googleapis.com/maps/api/staticmap?size=500x500&center="+string)
+      match /^(?<location>\w*)$/ do |client, data, match|
+      	client.say(channel: data.channel, text: "Recuerde separar palabras con '_' https://maps.googleapis.com/maps/api/staticmap?size=500x500&center=#{match[:location]}")
       end
     end
   end
